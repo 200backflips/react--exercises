fa563299cbf995d1b465c36e320a465ec6915776
Fredrik Karlsson <fredrikkarlsson320@gmail.com>
2026-02-19 15:50:07 +0100

    styling shenanigans

--- src/views/errors/beach.tsx
+++ src/views/errors/beach.tsx
@@ -13,14 +13,14 @@ export default function Beach({ textContent, imgUrl, badgeText }: Props) {
       <h4>Strandkort</h4>
       <Card className="w-fit">
         <CardContent className="flex flex-col gap-8">
-          <div className="relative">
+          <div className="relative max-w-84">
             <Badge variant="secondary" className="absolute top-0 m-2">
               {badgeText}
             </Badge>
             <img
               src={imgUrl}
               alt="en strand"
-              className="object-cover aspect-square max-w-80 rounded-md"
+              className="object-cover aspect-square rounded-md"
             />
           </div>
           <p className="w-[40ch]">{textContent}</p>
